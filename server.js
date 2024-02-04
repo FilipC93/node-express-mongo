fa5a06328f3b4055135bd1eec96b10ee0c4f71a9
Filip Cekani <66721991+FilipC93@users.noreply.github.com>
2024-02-05 00:40:45 +0200

    proper templates without routing added

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not organic');
+    return output;
+}
+
+const overview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const product = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
+const card = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
+
 //?Mini-api
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
@@ -12,11 +29,14 @@ const server = http.createServer((req, res) => {
     const pathName = req.url;
 
     if (pathName === '/' || pathName === '/overview') {
-        res.end('Hello from the overview tab');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        const cardsHTML = dataObj.map(e => replaceTemplate(card, e)).join('');
+        const output = overview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+        res.end(output);
     } else if (pathName === '/product') {
-        res.end('This is the product');
+        res.end(product);
     } else if (pathName === '/api') {
-        res.writeHead(200, { 'Content-type': 'application/json' }, {"set-cookie": [text='javascript']});
+        res.writeHead(200, { 'Content-type': 'application/json' }, { "set-cookie": [text = 'javascript'] });
         res.end(data);
     } else {
         res.writeHead(404, {
