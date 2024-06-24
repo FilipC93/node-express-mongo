267a47688e3d800580e3c33ef6c4ca96a0538ff6
Filip Cekani <66721991+FilipC93@users.noreply.github.com>
2024-06-24 19:19:15 +0300

    Dog API small course

---
+++ asynchronous/index.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePromise = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find the breed you asked for.');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not retrieve breed image');
+      resolve('successful');
+    });
+  });
+};
+
+//* The async & await way
+const getDogPic = async () => {
+  try {
+    const readData = await readFilePromise(`${__dirname}/dog.txt`);
+    const apiResult = await superagent.get(`https://dog.ceo/api/breed/${readData}/images/random`);
+    await writeFilePromise('dog-img.txt', apiResult.body.message);
+    console.log('Here is a pic of a random pupper');
+  } catch (err) {
+    console.log(err);
+  }
+};
+getDogPic();
+//?Modularized functions & better structure than callback hell
+// readFilePromise(`${__dirname}/dog.txt`)
+//   .then((data) => superagent.get(`https://dog.ceo/api/breed/${data}/images/random`))
+//   .then((res) => writeFilePromise('dog-img.txt', res.body.message))
+//   .then(() => console.log('Here is a random pupper'))
+//   .catch((err) => console.log(err.message));
+
+//? Callback hell version
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then((res) => {
+//       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+//         console.log('Here is a random pupper');
+//       });
+//     })
+//     .catch((err) => console.log(err.message));
+// });
