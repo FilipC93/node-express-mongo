14e70465da335fa1298c77eaeb89ad2cb0100f63
Filip Cekani <66721991+FilipC93@users.noreply.github.com>
2025-03-12 19:50:22 +0200

    established connection to MongoDB database

--- natours/server.js
+++ natours/server.js
@@ -1,8 +1,22 @@
+const mongoose = require('mongoose');
 //?If you want to run nodemon locally run the following:
 //* npx nodemon server.js OR npm start
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((connection) => {
+    console.log(connection.connections);
+    console.log('connection successful');
+  });
 const app = require('./app');
 
 //* Server start
